c4a8d0137ff8fc0cfdc74e7ac6b288759ab94999
mouloud madi <mouloutbk@gmail.com>
2021-11-02 15:33:37 +0100

    'correct-some-errors-in-group-model'

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,15 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\{
-    GroupUniverse,
-    GroupPost,
-    groupLink
-};
-use Auth;
+
 class Group extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'name',
         'user_id',
@@ -31,37 +27,21 @@ class Group extends Model
         'updated_at'
     ];
 
-
     public function universe()
     {
         return $this->belongsTo(GroupUniverse::class);
     }
 
-    /**
-     * Get the user that owns the Group
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    /**
-     * Get all of the comments for the Group
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
     public function posts()
     {
         return $this->hasMany(GroupPost::class);
     }
 
-    /**
-     * Get the user associated with the Group
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasOne
-     */
     public function linkInformation()
     {
         return $this->hasMany(groupLink::class);
