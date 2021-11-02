1fd46cb32cb375e0be6e65fc7c511abf3a926110
mouloud madi <mouloutbk@gmail.com>
2021-11-02 11:41:26 +0100

    'change-modal-GroupPost'

--- app/Models/GroupPost.php
+++ app/Models/GroupPost.php
@@ -4,13 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\{
-    GroupPostImage,
-    GroupPostLike,
-    User,
-    GroupPostComment,
-    Group
-};
 
 class GroupPost extends Model
 {
