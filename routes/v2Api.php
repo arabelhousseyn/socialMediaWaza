83fbdefe294df0c25b7daca3b5e703b4ac6961c4
arabelhousseyn <potency.football@gmail.com>
2021-11-10 08:28:15 +0000

    remove dubplicated changes

--- routes/v2Api.php
+++ routes/v2Api.php
@@ -98,28 +98,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     require_once __DIR__ . "/master/chat.php";
     // report
     Route::resource('report', ReportController::class);
-
-    // user
-    Route::get('usersByPhone/{string?}', [userController::class, 'getUsersByPhone']);
-    Route::get('getNotificationsNotRead', [NotificationController::class, 'getNotificationsNotRead']);
-    Route::resource('follow', FollowerController::class);
-    // groups and posts
-    Route::post('handleActionPost', [GroupPostController::class, 'hanldeAction2']);
-    Route::get('getOwnGroups', [GroupController::class, 'getOwnGroups']);
-    Route::get('getRandomGroups', [GroupController::class, 'getRandomGroups']);
-    Route::get('get-group-information/{id?}', [GroupController::class, 'getGroupInformation'])->whereNumber('id');;
-    Route::resource('group', GroupController::class);
-    Route::get('getposts/{group_id?}', [GroupPostController::class, 'getPostsbyGroup'])->whereNumber('group_id');
-    Route::resource('grouposts', GroupPostController::class);
-    Route::post('sharepost', [GroupPostController::class, 'sharePost']);
-    // search
-    Route::get('searchGlobal/{name?}', [userController::class, 'searchGlobal']);
-    Route::get('getNotifications', [NotificationController::class, 'getNotifications2']);
-    // notification
-    Route::get('getPureNotifcation/{notification_id}', [NotificationController::class, 'getPureNotifcation'])->whereNumber('notification_id');
-    Route::get('getNotificationById/{id}/{type}', [NotificationController::class, 'getNotificationById'])->whereNumber('id', 'type');
-    // report
-    Route::resource('report', ReportController::class);
 });
 
 
